374f2869fed32ac872a9252c773a2a5bb07922dc
GRADO EN INGENIERIA EN TECNOLOGIAS DE TELECOMUNICACION <0330717@lab.it.uc3m.es>
2017-05-05 14:15:54 +0200

    Cather modificado

--- AUROR_TESTS/catcher.py
+++ AUROR_TESTS/catcher.py
@@ -4,24 +4,24 @@ from __future__ import generators
 
 #INternal classes are nested whithin the factory method to disallow direct access to them
 class Catcher(object):
-
-    types_catcher = []
-
-    #Create based on class name => Posibles modificaciones para mejor adaptación
-    def factory(type):
-            class Hardware(Catcher):
-                #Insert code
-
-            class Software(Catcher):
-                #Insert code
-
-            class Mix(Catcher):
-                #Insert code
-
-            if type == "Hardware": return Hardware()
-            if type == "Software": return Software()
-            if type == "Mix": return Mix()
-            assert 0, "Bad Catcher Test creation: " + type
+    #
+    # types_catcher = []
+    #
+    # #Create based on class name => Posibles modificaciones para mejor adaptación
+    # def factory(type):
+    #         class Hardware(Catcher):
+    #             #Insert code
+    #
+    #         class Software(Catcher):
+    #             #Insert code
+    #
+    #         class Mix(Catcher):
+    #             #Insert code
+    #
+    #         if type == "Hardware": return Hardware()
+    #         if type == "Software": return Software()
+    #         if type == "Mix": return Mix()
+    #         assert 0, "Bad Catcher Test creation: " + type
 
 
 #Use of PLOYMORFHIC FACTORIES. By this way, different types of factories can be subclassed from
@@ -43,4 +43,20 @@ class CatcherFactory(object):
         return CatcherFactory.factories[id].create()
     createCatcher = staticmethod(createCatcher)
 
-    
+
+class Catcher(object): pass
+
+class Hardware(Catcher):
+
+    class Factory:
+        def create(self): return Hardware()
+
+class Software(Catcher):
+
+    class Factory:
+        def create(self): return Software()
+
+class Mix(Catcher):
+
+    class Factory:
+        def create(self): return Mix()
