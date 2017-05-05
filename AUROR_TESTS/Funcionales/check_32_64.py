d19d2ab0e28ea8127099fa45d57c2a72ab0e7cfe
GRADO EN INGENIERIA EN TECNOLOGIAS DE TELECOMUNICACION <0330717@lab.it.uc3m.es>
2017-05-05 10:43:24 +0200

    script to check if a syste, os a 32 0r 64 bit architecture added

---
+++ AUROR_TESTS/Funcionales/check_32_64.py
@@ -0,0 +1,29 @@
+#Script to check if a system has an 64 or 32 architecture. By default it will be a 32-bit-architecture
+
+import os
+import subprocess
+
+def define_architecture():
+    process = subprocess.Popen(['uname', '-m'],
+    
+                                stdout = subprocess.PIPE,
+                                )
+    out_value = process.communicate()[0]
+
+    out_value = out_value [:-1]
+
+    if out_value == 'x86_64':
+        type = 1 #64 bytes
+    else:
+        type = 0
+
+    info = [type, out_value]
+
+    return info
+
+def main():
+
+    print define_architecture()
+
+if __name__ == '__main__':
+    main()
