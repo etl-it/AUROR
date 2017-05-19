31866d99dd11f157672031a527c17a8ee0cf028f
GRADO EN INGENIERIA EN TECNOLOGIAS DE TELECOMUNICACION <0330717@lab.it.uc3m.es>
2017-05-19 12:30:19 +0200

    Empiezo la máquina de estados para definir el comportamiento de AUROR

---
+++ Abstraction/state_machine.py
@@ -0,0 +1,20 @@
+
+from time import sleep
+
+#Variable global
+state = 'i'
+
+def INICIO(start,config):
+    global state
+
+    print('Initial State')
+    #Transiciones
+    sleep(2)
+
+def CONFIG(start,config):
+
+def BASIC_TEST():
+
+def MY_TEST():
+
+def REPORT():
