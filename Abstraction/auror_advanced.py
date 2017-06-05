8f7b342455c3f443b3405a775add5dfa51ffb222
GRADO EN INGENIERIA EN TECNOLOGIAS DE TELECOMUNICACION <0330717@lab.it.uc3m.es>
2017-06-05 15:23:32 +0200

    Llamo ahora al config parser como cparser.set; me obloga a crear las secciones antes

--- Abstraction/auror_advanced.py
+++ Abstraction/auror_advanced.py
@@ -46,7 +46,7 @@ def configuration_file_style():
     print("AUROR CONFIGURATION FILE STYLE")
     print """
         [BASIC]
-        SeverityLevel = '1/2/3' [LOW/MEDIUM/HIGH]
+        SeverityLevel = 'l/m/h' [LOW/MEDIUM/HIGH]
         Report = 'y/n' [YES/NO]
 
         [TESTS]
@@ -79,20 +79,22 @@ def config(config_file):
             with open(config_file, 'wb') as archivo:
                 configuration_file_style() #Se muestra al usario la fomra que debe tener el fihchero de configuracion
 
-                severity = raw_input('Seleccione el grado de profundidad de los tests: >> ')
-                if severity.lower() == 'h':
-                    cparser['BASIC']['SeverityLevel'] = 'LOW'
-                elif severity == 2:
-                    cparser['BASIC']['SeverityLevel'] = 'MEDIUM'
-                elif severity == 3:
-                    cparser['BASIC']['SeverityLevel'] = 'HIGH'
+                cparser.add_section('BASIC')
+                cparser.add_section('TESTS')
 
+                severity = raw_input('Seleccione el grado de profundidad de los tests: >> ')
+                if severity.lower() == 'l':
+                    cparser.set('BASIC', 'SeverityLevel', 'LOW')
+                elif severity == 'm':
+                    cparser.set('BASIC', 'SeverityLevel', 'MEDIUM')
+                elif severity == 'h':
+                    cparser.set('BASIC', 'SeverityLevel', 'HIGH')
 
                 report = raw_input("Desea guardar un informe? >> ")
                 if report.lower() == 'y':
-                    cparser['BASIC']['Report'] = 'YES'
+                    cparser.set('BASIC', 'Report', 'YES')
                 elif report.lower() == 'n':
-                    cparser['BASIC']['Report'] = 'NO'
+                    cparser.set('BASIC', 'Report', 'NO')
 
                 number_of_tests = raw_input('Introduzca el numero de test que quiere llevar a cabo: >> ')
 
@@ -100,10 +102,10 @@ def config(config_file):
                     available_tests()
                     test = raw_input('Introduzca el identificador del test que quiere realizar: >>')
                     if test == 1:
-                        cparser['TESTS']['Test1'] = 'Connectivity'
+                        cparser.set('TESTS', 'Test1', 'Connectivity')
                     elif test == 2:
-                        cparser['TESTS']['Test2'] = 'Architecture'
-                
+                        cparser.set('TESTS', 'Test1', 'Architecture')
+
                 cparser.write(archivo) #Se escribe el archivo de configuracion
         else:
             exit() #Finalizo la aplicacion si el usuario no quiere configurar nada
