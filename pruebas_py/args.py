1dc6e12b162ca14f2ac6948c22bf429f3e4c87fa
N3m3sis <100330717@alumnos.uc3m.es>
2017-06-09 16:31:30 +0200

    Delete args.py

--- pruebas_py/args.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-##Con esto se consigue imprimir todas las variables almacenadas en la lista
-##'sys.argv'
-
-##El bucle for se ejecuta tantas veces como elementos tenga la lista
-
-for elements in sys.argv:
-    print elements
