f35a2251d9918eebd2190f7833eee8526005f920
GRADO EN INGENIERIA EN TECNOLOGIAS DE TELECOMUNICACION <0330717@lab.it.uc3m.es>
2017-06-15 12:08:20 +0200

    ...

--- Abstraction/connectivity_menu_config.py
+++ Abstraction/connectivity_menu_config.py
@@ -3,4 +3,7 @@ import os
 def menu_connectivity():
     print("CONNECTIVITY AUROR TEST CONFIGURATION")
     print """
-    
+        This test allows you testing whether another system is reachable over one or more hosts of your network.
+        To can configure the host you wanna test to.
+
+    """
