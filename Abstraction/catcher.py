faea923bc1fe7031bcef617cc031f2a4811c989f
GRADO EN INGENIERIA EN TECNOLOGIAS DE TELECOMUNICACION <0330717@lab.it.uc3m.es>
2017-06-21 11:07:40 +0200

    ...

--- Abstraction/catcher.py
+++ Abstraction/catcher.py
@@ -43,6 +43,7 @@ class MixCatcher(Catcher):
     def getId(self):
         return Catcher.getId()
     def catch(self): pass
+	print("esto es para quitar")
     def catch_with_report(self, id): pass
 
 class CatcherFactory:
