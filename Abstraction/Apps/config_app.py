ec824f353b41c2c00dbbe4a031c25b668de2ac90
GRADO EN INGENIERIA EN TECNOLOGIAS DE TELECOMUNICACION <0330717@lab.it.uc3m.es>
2017-06-26 12:58:28 +0200

    Añadido en app.conf la ruta para su fichero de configuración

--- Abstraction/Apps/config_app.py
+++ Abstraction/Apps/config_app.py
@@ -4,21 +4,24 @@ from COnfigParser import RawConfigParser
 def mode():
 
     mode = raw_input('Select if you want an only-one check package or if you wanna check several: One/More')
+    mode_opt = 0
 
     if mode.lower() == 'one' :
         select_one_pack()
+        mode_opt = 1
     elif mode.lower() == 'more' :
         select_sev_pack()
+        mode_opt = 2
     else:
         break
+
+    return mode_opt
 
 def select_one_pack():
 
     pack = raw_input('Insert the name of te package you wanna check version of:')
 
     return pack
-
-
 
 def select_sev_pack():
 
@@ -37,3 +40,37 @@ def select_sev_pack():
             end = True
 
     return pack_tests
+
+
+def config_app_conf(dir):
+
+    mode_opt = mode()
+
+    current_dir = os.getcwd()
+    config_app_conf = "/usr/lab/alum/0330717/Auror_workspace/app.conf"
+    cparser = RawConfigParser()
+
+    if current_dir != dir :
+        os.chdir(dir)
+
+    pack_tests = []
+
+    if mode_opt != 0:
+        if mode_opt == 1:
+            pack = select_one_pack()
+            pack_tests.append(pack)
+        elif mode_opt == 2:
+            pack_tests = select_sev_pack()
+    else:
+        break
+
+    with open(config_app_conf, 'wb') as archivo:
+        cparser.add_section("PACKAGES")
+        for pack un pack_tests:
+            pack_index = pack_tests.index(pack)
+            pack_name = 'PACK' + str(pack_index)
+            cparser.set("PACKAGES", pack_name, pack)
+        cparser.write(archivo)
+
+def main():
+    dir = "/usr/lab/alum/0330717/Auror_workspace"
